7e5b6c2861c2961465e56c4b7224c6222a62b817
Krzysztof Marciniak <krzysztofmarciniak881106@gmail.com>
2022-11-29 20:29:11 +0100

    Lekcja 2

--- hello_world.rb
+++ hello_world.rb
@@ -22,7 +22,7 @@ p 'ala' + ' ' + 'kot' # String
 p true # Bool
 p -14 # Integer
 p 2.7 # Float
-p 2,7
+p 2,7 # dwa Integery oddzielone przecinkiem
 
 # -----------------------------------
 
@@ -30,3 +30,43 @@ p [2,3,4,5]
 p 5 + 2
 p [2,3,4,5].sum
 p ['a', 34, 'text', ' ', 2]
+
+# -------------- lekcja 2
+p "lekcja 2"
+# -------------- operatory
+p 3 + 4 # 7  
+p 12 - 7 # 5
+p 3 * 4 # 12
+p 15 / 3 # 5
+p 5 / 2 # 2
+p 5.0 / 2 # 2.5
+p 5 % 2 # 1
+
+# -------------- operatory  porównania
+p 5 > 3 # true
+p 5 < 3 # false
+p 6 == 6 # true
+p 5 == 6 # false
+p 5 != 6 # true
+p 3 != 3 # false
+
+
+# ----- instrukcja warunkowa (ify, elsy)
+if 5 > 4 
+    p 15
+else
+    p 30
+end
+# wypisze się 15
+
+
+zmienna = 1
+p zmienna # 1
+zmienna = zmienna + 2
+p zmienna # 3
+if zmienna == 3
+    zmienna = zmienna + 3
+else
+    zmienna = zmienna * 3
+end
+p zmienna # 6
