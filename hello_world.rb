85527c3ddcb3423f722af23a247bcb86bd50c19e
Krzysztof Marciniak <krzysztofmarciniak881106@gmail.com>
2022-11-29 19:29:29 +0100

    lekcja 1

---
+++ hello_world.rb
@@ -0,0 +1,32 @@
+p "Hello World"
+p "hi"
+p 'kasztan'
+p '15'
+
+# -----------------------------------
+
+x = 17
+
+p x # Integer
+p 'x' # String
+p 23
+p 'x + 26' # String
+p x + 26 # Integer
+p 2 + 3 # Integer
+p "2" + "3" # String
+p 'ala' + 'kot' # String
+p 'ala' + ' ' + 'kot' # String
+
+# -----------------------------------
+
+p true # Bool
+p -14 # Integer
+p 2.7 # Float
+p 2,7
+
+# -----------------------------------
+
+p [2,3,4,5]
+p 5 + 2
+p [2,3,4,5].sum
+p ['a', 34, 'text', ' ', 2]
