8fc576ea74690ba710c7e8399d70bf76524662a5
Krzysztof Marciniak <krzysztofmarciniak881106@gmail.com>
2023-01-31 20:09:43 +0100

    31.01.2023

---
+++ Kotlet/frytki.rb
@@ -0,0 +1,23 @@
+class Frytki
+  def self.wydaj
+    return 'gotowe frytki'
+  end
+  def wyrzuć
+    return 'smietnik'
+  end
+end
+
+# p Frytki
+# p Frytki.new
+
+
+# def wydaj
+#   return "rakieta wystartowała"
+# end
+
+# p wydaj
+
+
+p Frytki.new.wyrzuć
+
+p Frytki.wydaj
--- Kotlet/kotlet_test.rb
+++ Kotlet/kotlet_test.rb
@@ -2,12 +2,16 @@ require 'minitest/autorun'
 
 class KotletTest < Minitest::Test
   def test_kotlet_działa
+    skip
     assert_equal 4, Kotlet.new.ubij(2)
+  end
+  def test_bazowy_kotlet
+    assert_equal 'start', Kotlet.startuj
   end
 end
 
 class Kotlet
   def ubij(_, _)
-    ubij = 4
+    # ubij = 4
   end
 end
