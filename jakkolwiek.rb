adb94503f05dec127f45110f2bf57dcfa105fb95
Krzysztof Marciniak <krzysztofmarciniak881106@gmail.com>
2023-03-23 20:17:22 +0100

    23.03.2023

---
+++ jakkolwiek.rb
@@ -0,0 +1,67 @@
+class HelloWorld
+  def self.hello(kotlet)
+    kotlet
+  end
+
+  def self.wyjaśnienie(frytki:)
+    frytki
+  end
+end
+
+p HelloWorld.hello(2) # 8
+p HelloWorld.hello(9) # 8
+
+p HelloWorld.wyjaśnienie(frytki: 15)
+p HelloWorld.wyjaśnienie(frytki: 19)
+
+
+#  Przykład ilstrujący, że funkcja zwraca ostatnią wartość
+# def testowa_metoda
+#   p 25
+#   36
+#   47
+#   testowa_zmienna = 13
+#   2
+#   p 5
+#   7
+#   testowa_zmienna
+# end
+
+# testowa_metoda
+# p "Oddychaj"
+# testowa_metoda
+# p "Oddychaj"
+# p testowa_metoda
+
+# nowa_zmienna = testowa_metoda
+
+
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# p nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
+# nowa_zmienna
