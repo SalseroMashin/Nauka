c3c7b1ae63bafc934cc55266476e7accfbab1635
Krzysztof Marciniak <krzysztofmarciniak881106@gmail.com>
2024-03-21 19:31:24 +0100

    21.03.2024

---
+++ 12_leap/leap.rb
@@ -0,0 +1,29 @@
+def przestępny (_)
+  rok = _
+  p rok
+  if rok == 2024 || rok == 2008
+    true
+  elsif rok == 2004
+    true
+  else
+    false
+  end
+end
+
+
+
+p przestępny (2024)
+
+p przestępny (2023)
+
+p przestępny (2025)
+
+p przestępny (2001)
+
+p przestępny (1999)
+
+p przestępny (1823)
+
+p przestępny (2004)
+
+p przestępny (2008)
