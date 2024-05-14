e3fe7bbbce9095dee1dd848d24c9258043058397
Krzysztof Marciniak <krzysztofmarciniak881106@gmail.com>
2024-05-14 20:32:22 +0200

    14.05.2024

---
+++ 12_leap/test_1405.rb
@@ -0,0 +1,14 @@
+nazwisko2 = "Sulka"
+nazwisko1 = "Warpas"
+imię = "Dawid"
+
+# p "#{imię} #{nazwisko}"
+
+nazwisko1 = "Marciniak"
+nazwisko2 = "Tychulski"
+imię = "Krzysiek"
+
+
+# p "\#{imię}\""
+
+p "#{imię} #{nazwisko1}-#{nazwisko2}"
