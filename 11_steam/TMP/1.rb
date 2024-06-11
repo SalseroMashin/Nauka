eb0041130913c0357844c5b6b2b94aebfa4468ef
Krzysztof Marciniak <krzysztofmarciniak881106@gmail.com>
2024-06-11 10:49:07 +0200

    11.06.2024

--- 11_steam/TMP/1.rb
+++ 11_steam/TMP/1.rb
@@ -2,6 +2,17 @@ liczba = 6
 
 p 4.times.class
 p 4.times
+p 4.times.class.ancestors
+
+p [1,3,8]
+p [1,3,8].class
+p [1,3,8].class.ancestors
+[1,3,8].each do
+  p liczba * 7
+end
+
+# p Object.methods
+# p Object.new.methods
 
 liczba.times do
   p liczba
