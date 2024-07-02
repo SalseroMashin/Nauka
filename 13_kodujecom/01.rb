6f822c6281974eee23cd15b41107dd4a90f24eea
root <root@SalseroAsus>
2024-07-02 20:00:42 +0200

    02.07.2024

---
+++ 13_kodujecom/01.rb
@@ -0,0 +1,16 @@
+class Nowa
+    def metoda(x)
+        "Cześć #{x}"
+    end
+end
+
+# p metoda(Dawid)
+# p metoda("Dawid")
+# @Nowa
+# p @Nowa
+# @metoda
+# @metoda("Dawid")
+# p Nowa
+# p @Nowa.metoda("Dawid")
+# p Nowa.metoda("Dawid")
+p Nowa.new.metoda("Dawid")
