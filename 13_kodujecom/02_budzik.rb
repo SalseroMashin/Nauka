42c44f7265962c4ff0cd349c5600a21cedf1b471
root <root@SalseroAsus>
2024-08-06 19:41:46 +0200

    06.08.2024

--- 13_kodujecom/02_budzik.rb
+++ 13_kodujecom/02_budzik.rb
@@ -25,50 +25,31 @@ p odczytaj_godzinę
 
 
 def test_ustawienia_budzika
-    p "test_ustawienia_budzika"
+    p "wykonuje się: test_ustawienia_budzika"
     budzik = Budzik.new
     budzik.ustaw_budzik_na("20:30")
     budzik.ustaw_budzik_na("21:30")
     # p budzik.powiedz_kiedy_zadzwoni
+    p "po lewej jest: budzik.powiedz_kiedy_zadzwoni"
+    p "po prawej jest: 21:30"
+    p "wynik porównania:"
     p ((budzik.powiedz_kiedy_zadzwoni) == ("21:30"))
 end
 
-test_ustawienia_budzika
-
 def test_ustawienia_dwóch_budzików
-    p "test_ustawienia_dwóch_budzików"
+    p "wykonuje się: test_ustawienia_dwóch_budzików"
     budzik_1 = Budzik.new
     budzik_2 = Budzik.new
     budzik_1.ustaw_budzik_na("08:00")
     budzik_2.ustaw_budzik_na("17:15")
+    p "08:00"
     p budzik_1.powiedz_kiedy_zadzwoni == "08:00"
+    p "17:15"
     p budzik_2.powiedz_kiedy_zadzwoni == "17:15"
 end
 
-test_ustawienia_dwóch_budzików
 test_ustawienia_budzika
-test_ustawienia_budzika
-test_ustawienia_dwóch_budzików
-test_ustawienia_budzika
-test_ustawienia_budzika
-test_ustawienia_dwóch_budzików
-test_ustawienia_budzika
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_budzika
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
-test_ustawienia_dwóch_budzików
+
 test_ustawienia_dwóch_budzików
 
 p Time.now
