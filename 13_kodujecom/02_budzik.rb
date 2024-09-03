88fdfaa4e45480e663d0790adbea0e6078606ca1
Krzysztof Marciniak-Tychulski <krzysztofmarciniak881106@gmail.com>
2024-09-03 20:31:52 +0200

    03.09.2024

--- 13_kodujecom/02_budzik.rb
+++ 13_kodujecom/02_budzik.rb
@@ -9,7 +9,8 @@ class Budzik
     end
 
 end
-    def odczytaj_godzinę
+
+def odczytaj_godzinę
         "19:34"
     end
 
@@ -23,20 +24,25 @@ p odczytaj_godzinę
 # p ustaw_budzik_na("20:30")
 # p ustaw_budzik_na("21:30")
 
-
 def test_ustawienia_budzika
+p "--------------------------------------------------------"
+p "&&&&&&&&&&&&&&&&&&&|START TEST_PIERWSZY|&&&&&&&&&&&&&&&&"
     p "wykonuje się: test_ustawienia_budzika"
     budzik = Budzik.new
     budzik.ustaw_budzik_na("20:30")
     budzik.ustaw_budzik_na("21:30")
     # p budzik.powiedz_kiedy_zadzwoni
+
     p "po lewej jest: budzik.powiedz_kiedy_zadzwoni"
     p "po prawej jest: 21:30"
     p "wynik porównania:"
     p ((budzik.powiedz_kiedy_zadzwoni) == ("21:30"))
+p "&&&&&&&&&&&&&&&&&&&&|END TEST_PIERWSZY|&&&&&&&&&&&&&&&&&"
+p "--------------------------------------------------------"
 end
 
 def test_ustawienia_dwóch_budzików
+p "****************|START_TEST_DRUGI|*********************"
     p "wykonuje się: test_ustawienia_dwóch_budzików"
     budzik_1 = Budzik.new
     budzik_2 = Budzik.new
@@ -46,10 +52,31 @@ def test_ustawienia_dwóch_budzików
     p budzik_1.powiedz_kiedy_zadzwoni == "08:00"
     p "17:15"
     p budzik_2.powiedz_kiedy_zadzwoni == "17:15"
+p "*****************|END_TEST_DRUGI|*********************"
+p "--------------------------------------------------------"
 end
 
 test_ustawienia_budzika
 
 test_ustawienia_dwóch_budzików
 
-p Time.now
+# p Time.now
+
+# p Time.now.methods
+
+# p Time.now.asctime
+
+# p Time.now.tuesday?
+
+# p Time.now.year
+
+# p Time.now.hour
+
+# p Time.now.min
+
+# p "20:24"
+
+def aktualna_godzina
+    "#{Time.now.hour}:#{Time.now.min}"
+end
+p aktualna_godzina
