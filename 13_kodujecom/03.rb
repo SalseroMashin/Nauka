a4ae22186f5fe679578dd5cfeddfb1ff965b3802
Krzysztof Marciniak-Tychulski <krzysztofmarciniak881106@gmail.com>
2024-10-15 19:59:07 +0200

    15.10.2024

--- 13_kodujecom/03.rb
+++ 13_kodujecom/03.rb
@@ -6,17 +6,18 @@ p song[:tytuł]
 
 p song[:wykonawcy]
 
+song_2 = {:tytuł => "Se Vende", :wykonawcy => "Formell, Los Van Van"}
+
 # 5.times do
 #     p song
 # end
 
-licznik = 0
+# licznik = 0
 
-while (licznik < 5)
-    p song
-    licznik = licznik + 1
-end
-
+# while (licznik < 5)
+#     p song
+#     licznik = licznik + 1
+# end
 
 playlista = []
 
@@ -26,6 +27,8 @@ p playlista.length
 
 playlista.push (song)
 
+playlista.push (song_2)
+
 p playlista
 
 p playlista.length
@@ -33,6 +36,12 @@ p playlista.length
 licznik_1 = 0
 
 while (licznik_1 < playlista.length)
-    p song
+    p licznik_1
+    # p song
+    # p playlista
+    # p playlista.at(0)
+    # p playlista.at(1)
+    p playlista.at(licznik_1)
     licznik_1 = licznik_1 + 1
+    # p licznik_1
 end
